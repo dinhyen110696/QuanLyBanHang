443671b0f5a66f825cf1b652a492869571243604
Huy Nguyen <huyithd205@gmail.com>
2017-05-30 21:50:38 +0700

    Kho va huong dan su dung

---
+++ Model/KhoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+  public  class KhoModel
+    {
+        public string MaKho { get; set; }
+        public string TenKho { get; set; }
+        public string ViTri { get; set; }
+        public string MaNV { get; set; }
+
+        public KhoModel(string ma,string ten,string vt,string manv)
+        {
+            MaKho = ma;
+            TenKho = ten;
+            ViTri = vt;
+            MaNV = manv;
+        }
+        public KhoModel() { }
+    }
+}
---
+++ QuanLyBanHang/TrangChu_UC/TroGiupUC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanHang.UC.TrangChu_UC
+{
+    public partial class TroGiupUC : UserControl
+    {
+        public TroGiupUC()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
